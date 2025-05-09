24082ea3806e40c21b146bd997192f371e5dec5f
Compressed <jameskorblox@gmail.com>
2025-05-08 20:32:07 -0400

    Create Hook.cpp

---
+++ Internal/Hook.cpp
@@ -0,0 +1,7 @@
+typedef void(__fastcall* RakSendType)(void*, void*, int, int, int, void*);
+RakSendType original_send;
+
+void __fastcall HookedSend(void* this_ptr, void* stream, int priority, int reliability, int ordering_channel, void* system_address) {
+    CallGlobalLuaFunction("_G.OnRakPeerSend", stream, priority, reliability, ordering_channel, system_address);
+    return original_send(this_ptr, stream, priority, reliability, ordering_channel, system_address);
+}
